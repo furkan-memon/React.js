fe1ef9b18794de1f7cc57257402d1b0b714918b9
furkan_memon <furkanmemon860@gmail.com>
2025-12-31 17:54:38 +0530

    Update Priority.jsx

--- 17-To-Do-list/src/component/Priority.jsx
+++ 17-To-Do-list/src/component/Priority.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const Priority = (props) => {
-    // const Onchange = () => {
-     
-    // }
+   
   return (
     <div>
       <select  onChange = {(e)=>{
-        props.setPriorityse(e.target.value)
-      }}  defaultValue="Prioritys" value={props.Priorityes} className='block bg-white text-black w-full border-gray-200 rounded-lg py-3 pl-3 pr-'>
+        props.setPriority(e.target.value)
+      }}   value={props.Priority} className='block bg-white text-black w-full border-gray-200 rounded-lg py-3 pl-3 pr-'>
              
-            <option  disabled value="Prioritys">Prioritys</option>
+            <option  value=""  disabled>Prioritys</option>
             <option value="Top Priority">Top Priority</option>
             <option  value="Mid Priority">Mid Priority</option>
             <option value="Low Priority">Low Priority</option>
