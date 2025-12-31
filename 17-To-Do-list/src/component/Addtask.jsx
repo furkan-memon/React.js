655e42835311cfe01aa86266c209ecbee1c0d286
furkan_memon <furkanmemon860@gmail.com>
2025-12-31 17:53:36 +0530

    Refactor Addtask component to use props

--- 17-To-Do-list/src/component/Addtask.jsx
+++ 17-To-Do-list/src/component/Addtask.jsx
@@ -1,53 +1,49 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 import Taskinp from './Taskinp'
 import Priority from './Priority'
 import DateInput from './DateInput'
 import Addtaskbtn from './Addtaskbtn'
-const Addtask = ({setSavedTask}) => {
-  const [Task, setTask] = useState('')
-  const [Priorityse, setPriorityse] = useState('')
-  const [date, setDate] = useState('')
+const Addtask = (props) => {
 
- 
-  const submitform = (e) =>{
-  e.preventDefault()
-  if (Task=='' || Priorityse == '' || date == '')
-  {
- alert('fill the input')
-  }else{
- setSavedTask(prev =>[
-    ...prev,
-    {
-    Task:Task,
-    Priority:Priorityse,
-    Date:date
-}
-  
- ])
+  const submitform = (e) => {
+    e.preventDefault()
+   
+    if (props.Task == '' || props.Priorityse == '' || props.date == '') {
+      alert('fill the input')
+    } else {
+      props.setSavedTask(prev => [
+        ...prev,
+        {
+          id: Date.now(),
+          Task: props.Task,
+          Priority: props.Priorityse,
+          Date: props.date
+        }
 
+      ])
 
-
-      setPriorityse('')
-      setDate('')
-      setTask("")
-}
-  }
-      
-      
+      props.setPriorityse('')
+      props.setTask('')
+      props.setDate('')
+    }
     
-     
-       
-    return (
-    <div className='rounded text-white flex flex-col border-2 border-white py-5 px-5'>
-        <form onSubmit={submitform} className='flex flex-col justify-between gap-4'>
+  }
 
-        <Taskinp Task={Task} setTask={setTask} />
-        <Priority Priorityse={Priorityse} setPriorityse={setPriorityse}/>
-        <DateInput Date={date} setDate={setDate}/>
-       <Addtaskbtn />
-        
-        </form>
+
+
+  
+
+
+  return (
+    <div className='rounded text-white flex flex-col border-2 border-white py-5 px-5'>
+      <form onSubmit={submitform} className='flex flex-col justify-between gap-4'>
+
+        <Taskinp Task={props.Task} setTask={props.setTask} />
+        <Priority Priority={props.Priorityse} setPriority={props.setPriorityse} />
+        <DateInput date={props.date} setDate={props.setDate} />
+        <Addtaskbtn />
+      </form>
     </div>
   )
 }
