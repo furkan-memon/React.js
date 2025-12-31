3bda738803482d18f2e3c3463ea5092c9c893396
furkan_memon <furkanmemon860@gmail.com>
2025-12-31 17:54:03 +0530

    Update Addtaskbtn.jsx

--- 17-To-Do-list/src/component/Addtaskbtn.jsx
+++ 17-To-Do-list/src/component/Addtaskbtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Addtaskbtn = () => {
   return (
-    <div> <button className='bg-white w-full text-black py-3 px-6 rounded active:scale-98'> Add Task</button></div>
+    <div> <button type='submit' className='bg-white w-full text-black py-3 px-6 rounded active:scale-98'> Add Task</button></div>
   )
 }
 
